4a45a7023c5bc35011ce2c647375e4f8c9fe2389
Nathan Zadoks <nathan@nathan7.eu>
2013-01-23 20:22:56 +0100

    style adjustments

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-var fs = require('fs');
-var path = require('path');
-var exports = module.exports = fsobj;
+var exports = module.exports = fsobj
+  , fs = require('fs')
+  , path = require('path')
 
 function fsobj(pathname, postfix) {
-    var obj = {};
-    var files = fs.readdirSync(pathname);
+    var obj = {}
+      , files = fs.readdirSync(pathname)
     files.forEach(function(fname) {
-        var fpath = path.join(pathname, fname);
-        var stats = fs.statSync(fpath);
-        var getter;
-        var propname = fname;
+        var fpath = path.join(pathname, fname)
+          , stats = fs.statSync(fpath)
+          , propname = fname
+          , getter
+
         if (stats.isFile()) {
-            getter = fs.readFileSync.bind(null, fpath, 'utf8');
-            if (postfix && fname.slice(-postfix.length) == postfix) propname = fname.slice(0, -postfix.length);
+            getter = fs.readFileSync.bind(null, fpath, 'utf8')
+            if (postfix && fname.slice(-postfix.length) == postfix) propname = fname.slice(0, -postfix.length)
         }
         else if (stats.isDirectory()) {
-            getter = fsobj.bind(null, fpath, postfix);
+            getter = fsobj.bind(null, fpath, postfix)
         }
 
         Object.defineProperty(obj, propname, {
             enumerable: true,
             configurable: true,
             get: getter
-        });
-    });
-    return obj;
+        })
+    })
+    return obj
 }
